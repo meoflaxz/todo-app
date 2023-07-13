dbb3eae8a8e8cb2acdf0a86e76baec738046d880
Meor Hasyim <meorhashim@gmail.com>
2023-07-13 22:01:25 +0800

    trying to add new_todo

--- gui.py
+++ gui.py
@@ -6,6 +6,23 @@ label = gui.Text("Type in a to-do")
 input_box = gui.InputText(tooltip="Enter todo")
 add_button = gui.Button("Add")
 
-window = gui.Window('My To-Do App', layout=[[label], [input_box, add_button]])
-window.read()
+window = gui.Window('My To-Do App',
+                    layout=[[label], [input_box, add_button]],
+                    font=('Helvetica', 20))
+
+while True:
+    event, values = window.read()
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            print(todos)
+            new_todo = values['todo'] + "\n"
+            print(new_todo)
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case gui.WIN_CLOSED:
+            break
+
+
 window.close()
+
