5ddbb15b2275bbccaac338dd90367da148d1a13e
Meor Hasyim <meorhashim@gmail.com>
2023-07-17 22:28:06 +0800

    implement clock with 200ms timeout update

--- gui.py
+++ gui.py
@@ -1,6 +1,8 @@
 import functions
 import PySimpleGUI as gui
+import time
 
+clock = gui.Text('', key='clock')
 label = gui.Text("Type in a to-do")
 input_box = gui.InputText(tooltip="Enter todo", key="todo")
 add_button = gui.Button("Add")
@@ -11,14 +13,16 @@ complete_button = gui.Button("Complete")
 exit_button = gui.Button("Exit")
 
 window = gui.Window('My To-Do App',
-                    layout=[[label],
+                    layout=[[clock],
+                            [label],
                             [input_box, add_button],
                             [list_box, edit_button],
                             [exit_button]],
                     font=('Helvetica', 10))
 
 while True:
-    event, values = window.read()
+    event, values = window.read(timeout=200)
+    window["clock"].update(value=time.strftime("%p %d, %Y %H:%M:%S"))
     print(event)
     print(values)
     match event:
@@ -28,25 +32,31 @@ while True:
             todos.append(new_todo)
             functions.write_todos(todos)
         case "Edit":
-            todo_to_edit = values['todos'][0]
-            new_todo = values['todo']
+            try:
+                todo_to_edit = values['todos'][0]
+                new_todo = values['todo']
 
-            todos = functions.get_todos()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo
-            functions.write_todos(todos)
-            window['todos'].update(values=todos)
+                todos = functions.get_todos()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo
+                functions.write_todos(todos)
+                window['todos'].update(values=todos)
+            except IndexError:
+                gui.popup("Please select an item first.", font=("Helvetica", 20))
         case 'Complete':
-            todo_to_complete = values['todos'][0]
-            todos = functions.get_todos()
-            todos.remove(todo_to_complete)
-            functions.write_todos(todos)
-            window['todos'].update(values=todos)
-            window['todo'].update(values='')
+            try:
+                todo_to_complete = values['todos'][0]
+                todos = functions.get_todos()
+                todos.remove(todo_to_complete)
+                functions.write_todos(todos)
+                window['todos'].update(values=todos)
+                window['todo'].update(values='')
+            except IndexError:
+                gui.popup("")
         case "Exit":
             break
         case 'todos':
-            window['todo'].update(values=values['todos'][0])
+            window['todo'].update(value=values['todos'][0])
         case gui.WIN_CLOSED:
             break
 
